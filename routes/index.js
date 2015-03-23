e77fea082c76a2ae01a809200ea3f02159c2865b
tango <tangodown1934@gmail.com>
2015-03-23 16:24:30 +0900

    implement google charts api

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var calc = require('../calc.js')
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,10 +10,15 @@ router.get('/', function(req, res, next) {
   res.json({success : 0});
 });
 
+/* Example */
 
 router.get('/calc', function(req, res, next){
   var sum = calc.sum(4,2);
   res.send("Result of sum : "+sum);
+});
+
+router.get('/charts', function(req, res, next){
+  res.render('charts', { title : 'charts' });
 });
 
 module.exports = router;
