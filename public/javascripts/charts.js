6c1d406ec3b79ada954a815ebaabbd2a4d9b9024
tango <tangodown1934@gmail.com>
2015-03-24 20:21:53 +0900

    clean google chart code

--- public/javascripts/charts.js
+++ public/javascripts/charts.js
@@ -1,20 +1,26 @@
 // Load the Visualization API and the piechart package.
-google.load('visualization', '1', {'packages':['corechart']});
+google.load("visualization", "1", {'packages':["corechart"]});
 
 // Set a callback to run when the Google Visualization API is loaded.
 google.setOnLoadCallback(drawChart);
 
 function drawChart() {
   var jsonData = $.ajax({
-    url: "data",
+    url: "/charts/data",
     dataType:"json",
     async: false
   }).responseText;
+
+  // Set chart options
+  var options = {'title':'Qufit Graph',
+    is3D: true,
+    'width':1000,
+    'height':1000};
 
   // Create our data table out of JSON data loaded from server.
   var data = new google.visualization.DataTable(jsonData);
 
   // Instantiate and draw our chart, passing in some options.
   var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-  chart.draw(data, {width: 1000, height: 1000});
+  chart.draw(data, options);
 }
